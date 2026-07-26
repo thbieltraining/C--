cd96446f8b994a5e409e41195cc72fc149d5ef14
Nathan Almeida <almeidanathan78@gmail.com>
2026-07-26 19:59:20 -0300

    Começando o terceiro semestre.
    Introdução a C++

---
+++ Introdução a C++.cpp
@@ -0,0 +1,6 @@
+// Vamo começar com uma declação de variáveis em C++ usando o padrão C++17. Aqui estão alguns exemplos de tipos de dados básicos:
+int idade = 20;        // número inteiro
+double altura = 1.75;   // número com casas decimais
+char letra = 'A';       // um único caractere
+bool ligado = true;     // verdadeiro ou falso
+// Note a diferença do python, onde não precisamos declarar o tipo da variável explicitamente. Em C++, é necessário especificar o tipo de dado ao declarar uma variável.
